35dc4d7d5dcf1b48e0f9c205d57a3de1a640afaf
Mjb141 <46346590+Mjb141@users.noreply.github.com>
2024-05-25 22:49:15 +0100

    Home 2024-04-25 22:47 : id 55 -

---
+++ home-manager/nvim.nix
@@ -0,0 +1,36 @@
+# This is your home-manager configuration file
+# Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
+{pkgs, ...}: let
+  neovim_config = ./files/nvim;
+in {
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    withNodeJs = true;
+    withPython3 = true;
+
+    extraPackages = with pkgs; [
+      alejandra
+      lua-language-server
+      nodePackages.bash-language-server
+      nodePackages.prettier
+      nodePackages.pyright
+      ruff
+      ruff-lsp
+      shellcheck
+      shfmt
+      stylua
+      terraform-ls
+      tflint
+      yaml-language-server
+    ];
+  };
+
+  # source lua config from this repo
+  xdg.configFile = {
+    "nvim" = {
+      source = "${neovim_config}";
+      recursive = true;
+    };
+  };
+}
