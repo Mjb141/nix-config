27f086478dd9f7b695c9a9ad1638b1e5abd5da8d
Mjb141 <46346590+Mjb141@users.noreply.github.com>
2024-06-04 13:09:57 +0100

    Nix 80 current  2024-06-04 13:09:52  24.11.20240531.57610d2   6.6.32                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -22,10 +22,17 @@
   security.rtkit.enable = true;
 
   # Core Settings
-  nix.settings = {
-    substituters = ["https://hyprland.cachix.org"];
-    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
-    experimental-features = ["nix-command" "flakes"];
+  nix = {
+    settings = {
+      substituters = ["https://hyprland.cachix.org"];
+      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+      experimental-features = ["nix-command" "flakes"];
+    };
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
   };
 
   sound.enable = true;
